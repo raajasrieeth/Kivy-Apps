3a14722a1b167b4391bc37dc1f19352f30384e21
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-08-12 08:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
+
+
 class MainApp(App):
     def build(self):
         self.operators = ["/", "*", "+", "-"]
